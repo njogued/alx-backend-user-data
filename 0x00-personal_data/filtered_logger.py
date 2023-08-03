5b44423261fdbeb227eb0b8c10391643afef6302
njogued <njogued@gmail.com>
2023-08-04 01:11:16 +0300

    Messages -> Message

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python3
 """A filter_datum function that logs obfuscated messages"""
 import re
+import logging
 
 
-def filter_datum(fields, redaction, messages, separator):
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
+
+
+def filter_datum(fields, redaction, message, separator):
     """Function to separate fields and redact info using regex"""
     return ';'.join([re.sub(r'({})=[^&]+'.format(i), f'{i}={redaction}', x)
                      if (i := x.split('=')[0]) in fields else x
-                     for x in messages.split(separator)])
+                     for x in message.split(separator)])
