68233cbe634fb96f18db456e003d4296c8dead5b
njogued <njogued@gmail.com>
2023-08-04 01:50:27 +0300

    try

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,33 +1,37 @@
 #!/usr/bin/env python3
 """A filter_datum function that logs obfuscated messages"""
 import re
-import logging
+# import logging
 
 
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
+# class RedactingFormatter(logging.Formatter):
+#     """ Redacting Formatter class
+#         """
 
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
+#     REDACTION = "***"
+#     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+#     SEPARATOR = ";"
 
-    def __init__(self):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
+#     def __init__(self):
+#         super(RedactingFormatter, self).__init__(self.FORMAT)
 
-    def format(self, record: logging.LogRecord) -> str:
-        NotImplementedError
+#     def format(self, record: logging.LogRecord) -> str:
+#         NotImplementedError
 
-
-def filter_datum(fields, redaction, message, separator):
-    """Function to separate fields and redact info using regex"""
-    for field in fields:
-        message = re.sub(rf"{field}=(.*?)\{separator}",
-                         f'{field}={redaction}{separator}', message)
-    return message
 
 # def filter_datum(fields, redaction, message, separator):
+#     """Function to separate fields and redact info using regex"""
+#     for field in fields:
+#         message = re.sub(rf"{field}=(.*?)\{separator}",
+#                          f'{field}={redaction}{separator}', message)
+#     return message
+
+def filter_datum(fields, redaction, message, separator):
     """ask gpt to explain"""
-    # return ';'.join([re.sub(r'({})=[^&]+'.format(i), f'{i}={redaction}', x)
-    #                  if (i := x.split('=')[0]) in fields else x
-    #                  for x in message.split(separator)])
+    return ';'.join([re.sub(r'({})=[^&]+'.format(i), f'{i}={redaction}', x)
+                     if (i := x.split('=')[0]) in fields else x
+                     for x in message.split(separator)])
+
+
+# if __name__ == '__main__':
+#     main()
