c65c82dda8ddc47e223e3abf14b22d586375b1b3
njogued <njogued@gmail.com>
2023-08-04 01:53:21 +0300

    Filtered Logger changed

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """A filter_datum function that logs obfuscated messages"""
+from typing import List
 import re
 # import logging
 
@@ -18,6 +19,13 @@ import re
 #     def format(self, record: logging.LogRecord) -> str:
 #         NotImplementedError
 
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """Returns a string with field obfuscated by redaction"""
+    for field in fields:
+        pattern = r'(?<={}=)(.+?)(?={})'.format(field, separator)
+        message = re.sub(pattern, redaction, message)
+    return message
 
 # def filter_datum(fields, redaction, message, separator):
 #     """Function to separate fields and redact info using regex"""
@@ -26,11 +34,11 @@ import re
 #                          f'{field}={redaction}{separator}', message)
 #     return message
 
-def filter_datum(fields, redaction, message, separator):
-    """ask gpt to explain"""
-    return ';'.join([re.sub(r'({})=[^&]+'.format(i), f'{i}={redaction}', x)
-                     if (i := x.split('=')[0]) in fields else x
-                     for x in message.split(separator)])
+# def filter_datum(fields, redaction, message, separator):
+#     """ask gpt to explain"""
+#     return ';'.join([re.sub(r'({})=[^&]+'.format(i), f'{i}={redaction}', x)
+#                      if (i := x.split('=')[0]) in fields else x
+#                      for x in message.split(separator)])
 
 
 # if __name__ == '__main__':
