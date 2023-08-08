5e3bad618a805b68f536197fd84fcbecb9ea23ad
njogued <njogued@gmail.com>
2023-08-09 01:29:21 +0300

    Check excluded paths

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,7 +9,16 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Routes that require authorization"""
-        return False
+        if not path or not excluded_paths:
+            return True
+        if path[-1] != "/":
+            slash_path = path + "/"
+        else:
+            slash_path = path
+        if path in excluded_paths or slash_path in excluded_paths:
+            return False
+        if path not in excluded_paths or slash_path not in excluded_paths:
+            return True
 
     def authorization_header(self, request=None) -> str:
         """Check creds in request header"""
