05d33d5734ff97b0ee096e192b4c6c82dacd9530
njogued <njogued@gmail.com>
2023-08-11 00:29:33 +0300

    Decoding the basic auth base64 object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """A BASICAUTH class that inherits from Auth
 Implements the Basic Authentication"""
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -16,3 +17,17 @@ class BasicAuth(Auth):
                     fields = authorization_header.split(" ")
                     return fields[-1]
         return None
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """Decode the base64 auth string in header"""
+        decoded = None
+        if base64_authorization_header:
+            if type(base64_authorization_header) is str:
+                try:
+                    decoded = base64.b64decode(base64_authorization_header)
+                    decoded = decoded.decode()
+                except:
+                    pass
+        return decoded
