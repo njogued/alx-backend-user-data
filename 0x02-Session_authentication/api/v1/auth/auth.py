9efd1f91c3c6195b7ee43491ccc905899607034f
njogued <njogued@gmail.com>
2023-08-11 01:39:21 +0300

    Method that returns a cookie value from a request

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -2,6 +2,7 @@
 """An authentication class for basic auth"""
 from flask import request
 from typing import List, TypeVar
+import os
 
 
 class Auth:
@@ -31,3 +32,11 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):
         """Return user object"""
         return None
+
+    def session_cookie(self, request=None):
+        """Method that returns a cookie value from a request
+        """
+        if request is None:
+            return None
+        session_name = os.getenv('SESSION_NAME')
+        return request.cookies.get(session_name)
