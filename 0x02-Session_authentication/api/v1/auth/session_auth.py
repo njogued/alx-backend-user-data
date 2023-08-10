6db7c9819333b679f0a021c338f83da4233cd5d8
njogued <njogued@gmail.com>
2023-08-11 01:30:24 +0300

    Create a session dictionary with userid as value

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
 """A module for session authentication"""
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """Session auth class that inherits from auth"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Method to create a session and Session ID for user"""
+        if user_id is None:
+            return None
+        if type(user_id) is not str:
+            return None
+        sess_id = uuid.uuid4()
+        SessionAuth.user_id_by_session_id[sess_id] = user_id
+        return sess_id
