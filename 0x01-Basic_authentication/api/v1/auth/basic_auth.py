387ccb19694f8cf43506984baca43ecdd9d6df70
njogued <njogued@gmail.com>
2023-08-11 00:52:18 +0300

    Find user from the db

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 Implements the Basic Authentication"""
 from api.v1.auth.auth import Auth
 import base64
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -43,3 +45,24 @@ class BasicAuth(Auth):
                     creds = (cred_list[0], cred_list[1])
                     return creds
         return (None, None)
+
+    def user_object_from_credentials(self,
+                                     user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """Return the user object from creds"""
+        email_dict = {}
+        email_dict['email'] = user_email
+        if user_email is None or user_pwd is None or type(user_email)\
+                is None or type(user_pwd) is None:
+            return None
+        try:
+            user = User.search(email_dict)
+            if user is None or user == []:
+                return None
+            else:
+                for one_user in user:
+                    if one_user.is_valid_password(user_pwd):
+                        return one_user
+                return None
+        except Exception:
+            return None
