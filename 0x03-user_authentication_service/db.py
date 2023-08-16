5c7a1ff1a714045b80bcc9fdfc40d4cf78ea0367
njogued <njogued@gmail.com>
2023-08-16 23:44:02 +0300

    Implementin g a find user function to filter user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,7 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 from user import Base, User
 
 
@@ -36,3 +37,14 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Implement the find user method to return user obj
+        raise invalid request if invalid arguments"""
+        if not kwargs:
+            raise InvalidRequestError
+        else:
+            user_obj = self._session.query(User).filter_by(**kwargs).first()
+            if not user_obj:
+                raise NoResultFound
+            return user_obj
