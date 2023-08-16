868a37a65e0e718b17d9a06632e9a5e22ecdff6f
njogued <njogued@gmail.com>
2023-08-17 00:08:42 +0300

    Handling exception raising

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,6 +4,7 @@ _hash_password: Function to hash password to bytes"""
 import bcrypt
 from db import DB
 from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 class Auth:
@@ -15,13 +16,14 @@ class Auth:
 
     def register_user(self, email: str, password: str) -> User:
         """Method to register a new user"""
-        if (self._db.find_user_by(email=email)):
-            raise ValueError(f"User {email} already exists")
-        else:
+        try:
+            if (self._db.find_user_by(email=email)):
+                raise ValueError(f"User {email} already exists")
+        except NoResultFound:
             hashed_password = _hash_password(password)
             new_user = self._db.add_user(
                 email=email, hashed_password=hashed_password)
-        return new_user
+            return new_user
 
 
 def _hash_password(password: str) -> bytes:
