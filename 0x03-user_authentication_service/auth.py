78cf3bce171c6b8048719b55dc1f9a28bd5a00f1
njogued <njogued@gmail.com>
2023-08-17 00:05:17 +0300

    Register a new user while doing password auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,7 @@
 _hash_password: Function to hash password to bytes"""
 import bcrypt
 from db import DB
+from user import User
 
 
 class Auth:
@@ -12,7 +13,15 @@ class Auth:
     def __init__(self):
         self._db = DB()
 
-    def register_user(self, email: str, password: str)
+    def register_user(self, email: str, password: str) -> User:
+        """Method to register a new user"""
+        if (self._db.find_user_by(email=email)):
+            raise ValueError(f"User {email} already exists")
+        else:
+            hashed_password = _hash_password(password)
+            new_user = self._db.add_user(
+                email=email, hashed_password=hashed_password)
+        return new_user
 
 
 def _hash_password(password: str) -> bytes:
