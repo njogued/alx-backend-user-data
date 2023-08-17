7d66aa33b8f6f5f464256161aa74e53598a197a7
njogued <njogued@gmail.com>
2023-08-17 13:51:11 +0300

    Handle the login logic

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Basic Flask application"""
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 
@@ -24,6 +24,20 @@ def register_user_route():
         return jsonify({"email": new_user.email, "message": "user created"})
     except Exception:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'])
+def login():
+    """Function to implement login logic"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if AUTH.valid_login(email, password):
+        sess_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie('session_id', sess_id)
+    else:
+        abort(401)
+    return response
 
 
 if __name__ == "__main__":
