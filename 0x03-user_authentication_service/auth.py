1b7f63b0a634b8bc33e725552b69acc2afe1eab0
njogued <njogued@gmail.com>
2023-08-17 14:57:52 +0300

    Auth method to update the password for a user with matching token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -75,6 +75,17 @@ class Auth:
         except NoResultFound:
             raise ValueError
 
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Update the password for user with reset token"""
+        try:
+            user_obj = self._db.find_user_by(reset_token=reset_token)
+            hashed_password = _hash_password(password)
+            self._db.update_user(user_obj.id, password=hashed_password)
+            self._db.update_user(user_obj.id, reset_token=None)
+            return None
+        except NoResultFound:
+            raise ValueError
+
 
 def _hash_password(password: str) -> bytes:
     """Hashing a password using bcrypt"""
