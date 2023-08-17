05abde1dfcf9404072623a4238de428bc3e1cc28
njogued <njogued@gmail.com>
2023-08-17 14:21:19 +0300

    Destroy session by deleting session id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -57,6 +57,14 @@ class Auth:
                 return None
         return None
 
+    def destroy_session(self, user_id: int) -> None:
+        """Method to destroy the user's current session"""
+        try:
+            self._db.update_user(user_id, session_id=None)
+        except NoResultFound:
+            return None
+        return None
+
 
 def _hash_password(password: str) -> bytes:
     """Hashing a password using bcrypt"""
