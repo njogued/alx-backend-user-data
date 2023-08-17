9a27bf5dba30160d87b39373eecfbbb109da9c0a
njogued <njogued@gmail.com>
2023-08-17 15:21:12 +0300

    Set id to autoincrement

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -8,7 +8,7 @@ Base = declarative_base()
 class User(Base):
     """Define the users table"""
     __tablename__ = 'users'
-    id = Column(Integer, nullable=False, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250))
